15928f2a14f35f7d62dc2d6730cc1ca595772c05
unknown <hybiekay2@gmail.com>
2025-07-29 16:45:14 +0100

    project added

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -2,8 +2,7 @@ import projects from "@/data/projects";
 import { notFound } from "next/navigation";
 import ProjectClientView from "./project-client-view";
 
-
-export default function ProjectDetail({ params }: { params: { slug: string } }) {
+export default async function ProjectDetail({ params }: { params: { slug: string } }) {
     const project = projects.find(p => p.slug === params.slug);
     if (!project) return notFound();
 
