2b1180526883c124da1c48c732d71986e2781dd7
unknown <hybiekay2@gmail.com>
2025-09-28 21:54:26 +0100

    Testing and add despose

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,13 +12,44 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-// export const metadata: Metadata = {
-//   title: "Ademola Ibukunoluwa Profile",
-//   description: "Welcome to the profile of Ademola Ibukunoluwa, a skilled developer and tech enthusiast.",
-//   keywords: "Ademola Ibukunoluwa, Profile, Developer, flutter Developer React Native, laravel Developer Tech Enthusiast, Portfolio",
-//   authors: [{ name: "Ademola Ibukunoluwa" }],
-//   viewport: "width=device-width, initial-scale=1.0",
-// };
+
+
+
+export const metadata: Metadata = {
+  title: "Ademola Ibukunoluwa | CodeWithHybiekay | Flutter Developer & Fullstack Engineer",
+  description:
+    "Hi, I'm Ademola Ibukunoluwa (Hybiekay) — Flutter Developer, Fullstack Engineer & Instructor. Learn coding, Flutter, and modern app development with tutorials and real-world projects.",
+  keywords:
+    "Flutter, Dart, Fullstack Developer, Portfolio, CodeWithHybiekay, Ibukunoluwa Ademola, Coding Tutorials",
+  authors: [
+    {
+      name: "Ibukunoluwa Ademola",
+      url: "https://www.linkedin.com/in/ademola-ibukunoluwa-572b77253",
+    },
+  ],
+  openGraph: {
+    title: "CodeWithHybiekay | Flutter Developer",
+    description: "Learn coding with Hybiekay — Flutter, Node.js, and more.",
+    url: "https://ibukunoluwa.ademola.ng",
+    type: "website",
+    images: [
+      {
+        url: "https://ibukunoluwa.ademola.ng/preview.jpg",
+        alt: "CodeWithHybiekay Preview",
+      },
+    ],
+    siteName: "CodeWithHybiekay",
+    emails: ["hybiekay2@gmail.com", "ibukun@eulogia.net",]
+  },
+  alternates: {
+    canonical: "https://ibukunoluwa.ademola.ng",
+  },
+  metadataBase: new URL("https://ibukunoluwa.ademola.ng"),
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 
 
 export default function RootLayout({
